7383c2a2408f5473e4686e7ec6661cc50e0d4329
Manthan1911 <patelmanthan2000@gmail.com>
2023-02-21 07:47:46 +0530

    modified positioning in home.html and changed some code in custom.js

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -1,5 +1,10 @@
 const searchButton = document.getElementById("search-button");
 const filterMissionNavbar = document.getElementById("filter-mission-navbar");
+
+console.log("custom");
+console.log(searchButton);
+console.log(filterMissionNavbar);
+
 let flag = 0;
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -21,30 +26,4 @@ function myFunction(x) {
 
 var x = window.matchMedia("(min-width: 576px)");
 myFunction(x);
-
-// ------------------------   toggle sidebar   -----------------------------------
-// const sidebarOpenBtn = document.querySelectorAll(".sidebar-open");
-// const sidebarCloseBtn = document.getElementById("sidebar-close");
-// const sidebar = document.getElementById("sidebar");
-
-// function myFunction2(y) {
-//   if (y.matches) {
-//     sidebar.setAttribute("style", "display:none !important");
-//   }
-// }
-
-// var y = window.matchMedia("(min-width: 768px)");
-// myFunction2(y);
-// y.addListener(myFunction2);
-
-// // let sidebarStatus = "close";
-
-// for (let i = 0; i < sidebarOpenBtn.length; i++) {
-//   sidebarOpenBtn[i].addEventListener("click", (e) => {
-//     sidebar.style.display = "block";
-//   });
-// }
-
-// sidebarCloseBtn.addEventListener("click", (e) => {
-//   sidebar.style.display = "none";
-// });
+x.addListener(myFunction);
